44c5c68b9672ee0971a07c2d822c0203a01ef350
Daples <daples1@gmail.com>
2023-03-24 21:52:49 +0100

    [ src ] Organized code and added additional tests.

--- src/tests.py
+++ src/tests.py
@@ -1,58 +1,29 @@
-from fem_students_1d import (
-    create_mesh,
-    create_param_map,
-    create_fe_space,
-    create_ref_data,
-)
+from utils.test_space import Tests
 
-import numpy as np
-import matplotlib.pyplot as plt
+L = 2
 
-m = 4
 p = 1
 k = 0
-L = 2
-Omega = [0, L]
-brk = np.linspace(0, L, m + 1)
+m = 4
+neval = 20
 
-mesh = create_mesh(brk)
-param_map = create_param_map(mesh)
-space = create_fe_space(p, k, mesh)
-n = space["n"]
-supported_bases = space["supported_bases"]
-extraction_coefficients = space["extraction_coefficients"]
+# Test B
+filename = "test_B.pdf"
+spacing_func = lambda i: i * L / m
+Tests.test_space(p, k, m, spacing_func, neval, filename)
 
-neval = 10
-deg = p
-integrate_flag = False
-ref_data = create_ref_data(neval, deg, integrate_flag)
-reference_basis = ref_data["reference_basis"]
-reference_basis_derivatives = ref_data["reference_basis_derivatives"]
-evaluation_points = ref_data["evaluation_points"]
+# Test C
+filename = "test_C.pdf"
+spacing_func = lambda i: i**2 * L / m**2
+Tests.test_space(p, k, m, spacing_func, neval, filename)
 
-fig, axs = plt.subplots(3, 2)
-for h, j in enumerate([0, 3, n - 1]):
-    arr = np.zeros((3, m * neval))
-    for l in range(mesh.elements.shape[1]):
-        element = mesh.elements[:, l]
-        lower_index = l * neval
-        upper_index = lower_index + neval
-        print(f"({lower_index}, {upper_index})")
-        arr[0, lower_index:upper_index] = param_map.func(
-            evaluation_points, element[0], element[1]
-        )
+# Test D
+filename = "test_D.pdf"
+spacing_func = lambda i: i * L / m
+p = 2
+Tests.test_space(p, k, m, spacing_func, neval, filename)
 
-        aux = supported_bases[l] == j
-        is_supported = aux.any()
-
-        if is_supported:
-            index_support = np.where(aux)[0][0]
-            ej_i = extraction_coefficients[l][index_support, :]
-            arr[1, lower_index:upper_index] = ej_i.dot(reference_basis)
-            arr[2, lower_index:upper_index] = param_map.imap_derivatives[l] * ej_i.dot(
-                reference_basis_derivatives
-            )
-
-    axs[h, 0].plot(arr[0], arr[1], label=f"{j}")
-    axs[h, 1].plot(arr[0], arr[2], label=f"{j}")
-plt.show()
+# Test E
+filename = "test_E.pdf"
+spacing_func = lambda i: i**2 * L / m**2
+Tests.test_space(p, k, m, spacing_func, neval, filename)
