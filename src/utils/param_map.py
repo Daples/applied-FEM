febc6d9c15e04108df0e091175e209d9c3fe3be3
Daples <daples1@gmail.com>
2023-04-07 11:35:13 +0200

    [ utils ] Added docstring to parametric map class.

--- src/utils/param_map.py
+++ src/utils/param_map.py
@@ -6,6 +6,21 @@ import numpy as np
 
 @dataclass
 class ParamMap:
+    """A data class to represent a parametric map.
+
+    Attributes
+    ----------
+    func: [
+        numpy.ndarray | float, numpy.ndarray | float, numpy.ndarray | float
+    ] -> numpy.ndarray | float
+        The map from the reference element to the actual element.
+
+    map_derivatives: numpy.ndarray
+        The derivatives of the map on the reference element.
+    imap_derivatives: numpy.ndarray
+        The derivatives of the inverse map.
+    """
+
     func: Callable[
         [np.ndarray | float, np.ndarray | float, np.ndarray | float], np.ndarray | float
     ]
