a4147486de702eda818c10f55eac989436da9de6
Daples <daples1@gmail.com>
2023-04-25 10:51:39 +0200

    [ src ]

--- src/ass3_exercise_G.py
+++ src/ass3_exercise_G.py
@@ -13,6 +13,15 @@ from utils import eval_func
 from utils.plotter import Plotter
 
 
+# Exact solutions
+u_e = lambda x: np.cos(np.pi * (x - 0.5))
+d_u_e = lambda x: -np.pi * np.sin(np.pi * (x - 0.5))
+sigma = lambda x: -d_u_e(x)
+d_sigma = lambda x: np.pi**2 * np.cos(np.pi * (x - 0.5))
+fs = [sigma, u_e]
+dfs = [d_sigma, d_u_e]
+
+
 def problem_B11(
     x: np.ndarray, Nj: np.ndarray, _: np.ndarray, Nk: np.ndarray, dNk: np.ndarray
 ) -> np.ndarray:
@@ -50,8 +59,8 @@ problem_Ls = [problem_L1, problem_L2]
 
 # Choice 1
 m = 14
-ps = [0, 1]
-ks = [-1, 0]
+ps = [2, 1]
+ks = [1, 0]
 L = 1
 neval = 3
 spacing_func = lambda i: i * L / m
@@ -74,3 +83,45 @@ A, b = assemble_fe_mixed_problem(
     mesh, spaces, ref_datas, param_maps, problem_B_mat, problem_Ls
 )
 x_sol = np.linalg.solve(A, b)
+
+ns = [space.dim for space in spaces]
+accum = 0
+labels = ["sigma", "u"]
+for i in range(len(ns) - 1):
+    space = spaces[i]
+    ref_data = ref_datas[i]
+    param_map = param_maps[i]
+    n = ns[i]
+    coefs = x_sol[accum : accum + n]
+    accum += n
+
+    # Recover solution
+    m = mesh.elements.shape[1]
+    q = ref_data.evaluation_points.shape[0]
+    xs_matrix = np.zeros((m, q))
+    ns_matrix = np.zeros_like(xs_matrix)
+    dxns_matrix = np.zeros_like(ns_matrix)
+
+    for current_element in range(mesh.elements.shape[1]):
+        element = mesh.elements[:, current_element]
+        xs, ns, dxns = eval_func(
+            current_element, coefs, element, param_map, space, ref_data
+        )
+        xs_matrix[current_element, :] = xs
+        ns_matrix[current_element, :] = ns
+        dxns_matrix[current_element, :] = dxns
+
+    # Plot
+    x_exact = np.linspace(0, L, m * q)
+    exact = fs[i](x_exact)
+    d_exact = dfs[i](x_exact)
+    Plotter.plot_results(
+        xs_matrix,
+        ns_matrix,
+        dxns_matrix,
+        f"ass_exercise_G_{labels[i]}.pdf",
+        result_label=f"{labels[i]}_h",
+        x_exact=x_exact,
+        exact_solution=exact,
+        d_exact_solution=d_exact,
+    )
