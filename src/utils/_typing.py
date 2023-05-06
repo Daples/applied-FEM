f96185a025c3ff0b7a70d12ea9ab5b691abc1fdd
Daples <daples1@gmail.com>
2023-05-06 12:27:52 +0200

    [ utils ] Added new generic types for (bi)linear forms.

--- src/utils/_typing.py
+++ src/utils/_typing.py
@@ -1,3 +1,7 @@
+from typing import Callable
 import numpy as np
 
 DataArray = list[float | int] | np.ndarray
+FormData = np.ndarray | float
+BilinearForm = Callable[[FormData, FormData, FormData, FormData, FormData], FormData]
+LinearForm = Callable[[FormData, FormData, FormData], FormData]
