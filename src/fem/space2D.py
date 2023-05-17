63b52ba606960f700ef9a0fe7c4fbc20e64be550
Daples <daples1@gmail.com>
2023-05-17 15:03:39 +0200

    [ fem ] Added 2D space.

---
+++ src/fem/space2D.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from fem.mesh import Mesh
+
+
+class SupportExtractor:
+    def __init__(
+        self, supported_bases: np.ndarray, extraction_coefficients: np.ndarray
+    ) -> None:
+        self.supported_bases: np.ndarray = supported_bases
+        self.extraction_coefficients: np.ndarray = extraction_coefficients
+
+
+class Space2D:
+    """A data class to represent the 2D finite element space.
+
+    Attributes
+    ----------
+    """
+
+    def __init__(
+        self,
+        n: int,
+        boundary_bases: np.ndarray,
+        support_extractors: list[SupportExtractor],
+    ) -> None:
+        self.n: int = 0
+        self.boundary_bases: np.ndarray = boundary_bases
+        self.support_extractors: list[SupportExtractor] = support_extractors
